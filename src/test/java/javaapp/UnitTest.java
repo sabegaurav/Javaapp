6ce29a22b1f9a6a2e4e7f814a7794c02beaa1e46
Gaurav <gauravsabe12@gmail.com>
2026-05-04 10:18:29 +0530

    changes unit test

---
+++ src/test/java/javaapp/UnitTest.java
@@ -0,0 +1,13 @@
+package javaapp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+
+	@Test
+	public void Test() {
+		
+		Assertions.assertEquals(true, false);
+	}
+}
